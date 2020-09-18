24c6b7f73943871b8cde577516b43c4edd7266ce
hatakiboy <199820grihan@gmail.com>
2020-09-18 16:46:43 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+menu.onclick = function myFunction() {
+  let x = document.getElementById('myTopnav');
+
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+};
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const blockID = anchor.getAttribute('href');
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
